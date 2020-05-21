2088ca9c5bf8f4c637cda38c36694d70c3115d13
rewrib <birwer.ali@gmx.de>
2020-05-21 15:49:32 +0200

    Add project files.

---
+++ NotesDemo/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace NotesDemo
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public MainPageViewModel()
+        {
+            AllNotes = new ObservableCollection<string>();
+            EraseCommand = new Command( () =>
+            { TheNote = string.Empty; }  );
+
+            SaveCommand = new Command(() =>
+            {
+                AllNotes.Add(TheNote);
+                TheNote = string.Empty;
+            }
+                );
+
+        }
+
+        public ObservableCollection<string> AllNotes { get; set; }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string theNote;
+
+        public string TheNote
+        {
+            get => theNote;
+            set
+            {
+                theNote = value;
+                var args = new PropertyChangedEventArgs(nameof(TheNote));
+                PropertyChanged?.Invoke(this, args);
+            }
+        }
+
+        public Command SaveCommand { get; }
+        public Command EraseCommand { get; }
+    }
+
+}
